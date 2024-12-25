82654297ba94a62ea7bc34d74c9a9a6547bd49f5
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2024-12-25 11:51:24 +0800

    Minor updates

--- website/src/pages/HomePage.js
+++ website/src/pages/HomePage.js
@@ -36,7 +36,7 @@ function HomePage(){
                 Welcome to my page! I’m currently a student at William & Mary studying computer science and applied mathematics. My interests center around machine learning, quantum computing, and computer architecture.
                 </div>
                 <div className = "description">
-                I am currently researching svbrdf image diffusion models and the ZX-calculus quantum optimization method as an undergraduate researcher at William & Mary. I was selected to represent the W&M Department of Computer Science at the 2024 undergraduate research fair for the latter optimization research.
+                I am currently researching SVBRDF image diffusion models and the ZX-calculus quantum optimization method as an undergraduate researcher at William & Mary. I was selected to represent the W&M Department of Computer Science at the 2024 undergraduate research fair for the latter optimization research.
                 </div>
                 <div className = "description">
                 This summer, I will be interning at Black Cape, Inc. as a Software Engineering Intern, where I previously interned last summer and am continuing part-time work.
@@ -48,7 +48,7 @@ function HomePage(){
                 You can learn more about me at the links below, and feel free to contact me at rhuang03@wm.edu!
                 </div>
                 <div id = "social-icons">
-                    <a href = "https://drive.google.com/file/d/14HHbadnG9c69UmuNF7i3ZLoBCKyB68n4/view?usp=sharing" target = "_blank">
+                    <a href = "https://drive.google.com/file/d/1utkBTLBYgq5sDnlKOukq9qRz3kWmQ6Rh/view?usp=sharing" target = "_blank">
                         <img src = {resumeIcon}/>
                     </a>
                     <a href = "https://www.linkedin.com/in/rachel-huang-b26211216/" target = "_blank">
