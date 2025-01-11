a3901060ec8ceb869924307786651b9549fba37f
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2025-01-11 23:51:22 +0800

    Temp updates

--- website/src/pages/HomePage.js
+++ website/src/pages/HomePage.js
@@ -33,17 +33,26 @@ function HomePage(){
             </div>
             <div id = "bottom-rectangle">
                 <div className = "description">
-                Welcome to my page! I’m currently a student at William & Mary studying computer science and applied mathematics. My interests center around machine learning, quantum computing, and computer architecture.
+                Welcome to my page! I'm currently a student at William & Mary studying computer science and applied mathematics. My interests center around machine learning, quantum computing, and computer architecture. 
                 </div>
                 <div className = "description">
-                I am currently researching SVBRDF image diffusion models and the ZX-calculus quantum optimization method as an undergraduate researcher at William & Mary. I was selected to represent the W&M Department of Computer Science at the 2024 undergraduate research fair for the latter optimization research.
+                I am currently contributing to the LLM apps and data pipelines of Black Cape, Inc where I worked full-time as a Software Engineering Intern last summer. I am also researching SVBRDF image diffusion models
+                as an undergraduate researcher at William & Mary, running experiments with the Matfusion image diffusion model with the goal of generating tileable SVBRDFs.
                 </div>
                 <div className = "description">
+                    I love exploring new technologies and projects and will often spend hours tinkering with a new idea. I have built several iterations of a personal website,
+                    trying to settle on a version that is modular and easily extendable. This current site will either grow or change again with equal likelihood as I continue in my
+                    other endeavors. Feel free to contact me at rhuang03@wm.edu!   
+                </div>
+                <div className = "description">
+                    In my free time, I am probably reading my growing collection of Marvel comics or feeding my addiction of action and mystery movies. 
+                </div>
+                {/* <div className = "description">
                 This summer, I will be interning at Black Cape, Inc. as a Software Engineering Intern, as well as continuing my Monroe Scholar funded quantum optimization research.
                 </div>
                 <div className = "description">
                 You can learn more about me at the links below, and feel free to contact me at rhuang03@wm.edu!
-                </div>
+                </div> */}
                 {/* <div id = "social-icons">
                     <a href = "https://drive.google.com/file/d/1eRf6NYuO6vVu0S23e2ja6PXjy49UcE59/view?usp=sharing" target = "_blank">
                         <img src = {resumeIcon}/>
@@ -55,17 +64,6 @@ function HomePage(){
                         <img src = {githubIcon}/>
                     </a>
                 </div> */}
-                <div id = "social-icons">
-                    <a href = "" target = "_blank">
-                        <img src = {resumeIcon}/>
-                    </a>
-                    <a href = "" target = "_blank">
-                        <img src = {linkedinIcon} id = "linkedin-icon"/>
-                    </a>
-                    <a href = "" target = "_blank">
-                        <img src = {githubIcon}/>
-                    </a>
-                </div>
             </div>
         </div>
     );
