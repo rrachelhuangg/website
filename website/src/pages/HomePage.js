8a4a2ef7f69150a611943aa2a677048263d1e0d4
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2025-06-18 21:17:42 -0400

    Redid projects page a bit and added IoTPacketPatrol page.

--- website/src/pages/HomePage.js
+++ website/src/pages/HomePage.js
@@ -1,17 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "../index.css";
-import resumeIcon from "../assets/images/resume.png";
-import linkedinIcon from "../assets/images/linkedin.png";
-import githubIcon from "../assets/images/github.png";
 import {Link} from "react-router-dom";
 import "../index.css";
-import presentationIcon from "../assets/images/presentation.jpg";
 
 function HomePage(){
     return(
         <div id = "card">
             <div id = "top-rectangle">
-                <div id = "name">hello! i'm rachel huang.</div>
+                <div id = "name">hello! i'm rachel huang.
+                </div>
             </div>
             <div id = "bottom-rectangle">
                 <div className = "description">
@@ -26,17 +23,6 @@ function HomePage(){
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
                     </iframe>
                 </div>
-                {/* <div id = "social-icons">
-                    <a href = "https://drive.google.com/file/d/1eRf6NYuO6vVu0S23e2ja6PXjy49UcE59/view?usp=sharing" target = "_blank">
-                        <img src = {resumeIcon}/>
-                    </a>
-                    <a href = "https://www.linkedin.com/in/rachel-huang-b26211216/" target = "_blank">
-                        <img src = {linkedinIcon} id = "linkedin-icon"/>
-                    </a>
-                    <a href = "https://github.com/rrachelhuangg" target = "_blank">
-                        <img src = {githubIcon}/>
-                    </a>
-                </div> */}
             </div>
         </div>
     );
--- website/src/pages/ProjectsPage.js
+++ website/src/pages/ProjectsPage.js
@@ -1,5 +1,9 @@
 import React, {useState} from "react";
 import "../index.css";
+import enigmaIcon from "../assets/images/enigma.png";
+import doodlespaceIcon from "../assets/images/doodlespace.png";
+import transparentgptIcon from "../assets/images/transparentgpt.png";
+import iotIcon from "../assets/images/iot.png";
 
 function ProjectsPage(){
     return(
@@ -9,21 +13,39 @@ function ProjectsPage(){
             </div>
             <div id = "bottom-rectangle">
                 <div className = "description">
-                    <a href="https://rrachelhuangg.github.io/enigma/" target="_blank" className = "nav-button">The Enigma Machine</a>
-                    <div className = "inner-description">
-                        implementation of the enigma encryption machine with animation and sound
+                    <img src={iotIcon}/>
+                    <div className="text-block">
+                        <a href="https://iotpacketpatrol.onrender.com/" target="_blank" className = "nav-button">IoTPacketPatrol - Google Cloud AI in Action hackathon, MongoDB track submission</a>
+                        <div className = "inner-description">
+                            full-stack machine learning model and ui for analyzing iot network traffic data
+                        </div>
                     </div>
                 </div>
                 <div className = "description">
-                    <a href="https://www.sprint.dev/projects/331b0fa1-47f8-4f67-bdb2-931ca2e157cb" target="_blank" className = "nav-button">TransparentGPT - 3rd Place at Rebuild AI Hackathon</a>
-                    <div className = "inner-description">
-                        transparent chatbot with exposed sources and customization panel
+                    <img src={enigmaIcon}/>
+                    <div className="text-block">
+                        <a href="https://rrachelhuangg.github.io/enigma/" target="_blank" className = "nav-button">The Enigma Machine</a>
+                        <div className = "inner-description">
+                            implementation of the enigma encryption machine with animation and sound
+                        </div>
                     </div>
                 </div>
                 <div className = "description">
-                    <a href="https://devpost.com/software/draw-j3nxl2?ref_content=my-projects-tab&ref_feature=my_projects" target="_blank" className = "nav-button">DoodleSpace - HooHacks2025 submission</a>
-                    <div className = "inner-description">
-                        microcontroller and sensor setup with data pipeline for rendering 3D vectors
+                    <img src={transparentgptIcon}/>
+                    <div className="text-block">
+                        <a href="https://www.sprint.dev/projects/331b0fa1-47f8-4f67-bdb2-931ca2e157cb" target="_blank" className = "nav-button">TransparentGPT - 3rd Place at Rebuild AI Hackathon</a>
+                        <div className = "inner-description">
+                            transparent chatbot with exposed sources and customization panel
+                        </div>
+                    </div>
+                </div>
+                <div className = "description">
+                    <img src={doodlespaceIcon}/>
+                    <div className="text-block">
+                        <a href="https://devpost.com/software/draw-j3nxl2?ref_content=my-projects-tab&ref_feature=my_projects" target="_blank" className = "nav-button">DoodleSpace - HooHacks2025 submission</a>
+                        <div className = "inner-description">
+                            microcontroller and sensor setup with data pipeline for rendering 3D vectors
+                        </div>
                     </div>
                 </div>
             </div>
