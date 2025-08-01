d5431f110eb50e19d9d6048cfd9e74a8aced520d
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2025-07-31 21:20:29 -0400

    More updates

--- website/src/pages/HomePage.js
+++ website/src/pages/HomePage.js
@@ -24,18 +24,25 @@ function HomePage(){
                         </div>
                     </div>
                     <div class="video-container">
-                    <div class="outline-container">
-                        <div className = "description">
-                            <iframe width="560" height="315"
-                                src="https://www.youtube.com/embed/K32J2yVN-XI"
-                                title="Buttons Youtube Video"
-                                frameborder="0"
-                                id = "buttons-video"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
-                            </iframe>
+                        <div class="outline-container">
+                            <div className = "description">
+                                <iframe width="560" height="315"
+                                    src="https://www.youtube.com/embed/K32J2yVN-XI"
+                                    title="Buttons Youtube Video"
+                                    frameborder="0"
+                                    id = "buttons-video"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
+                                </iframe>
+                            </div>
                         </div>
                     </div>
-                </div>
+                    <div class="second-video-container">
+                        <div class="outline-container">
+                            <div className = "description">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/v1FsujJONGA?si=036hiskXiN0MW0oB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="section-container">
                     <div className="home-container">
